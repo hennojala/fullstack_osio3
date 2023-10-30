19ab1c59a9c1d3c01007193285e4b0828e2b2b50
Henna Ojala <henna-ojala@outlook.com>
2023-10-30 13:00:16 +0200

    dist ja muuta

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 // Määritä mukautettu token morganille näyttämään POST-pyynnön datan.
 morgan.token('postData', (req, res) => {
